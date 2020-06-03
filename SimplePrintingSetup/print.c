1ba46147c49010fe07d5d8eac2d4a591d180fd4d
root <root@h2871437.stratoserver.net>
2020-06-03 16:56:13 +0200

    Add Simple Print-Functions

---
+++ SimplePrintingSetup/print.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int printInt(int a){
+	return printf("%d\n", a);
+}
+
+int printDouble(double c){
+	return printf("%.15f\n", c);
+}
+
+int printChar(char a){
+	return printf("%c\n", a);
+}
+
+
+
+
+
